2c84b81511ba901e2588acb26039ee4c0248b219
Imran <imranspahic300@gmail.com>
2022-06-23 10:55:28 +0200

    exported cursot style

--- lib/flutter_quill.dart
+++ lib/flutter_quill.dart
@@ -10,6 +10,7 @@ export 'src/models/themes/quill_custom_icon.dart';
 export 'src/models/themes/quill_dialog_theme.dart';
 export 'src/models/themes/quill_icon_theme.dart';
 export 'src/widgets/controller.dart';
+export 'src/widgets/cursor.dart';
 export 'src/widgets/default_styles.dart';
 export 'src/widgets/editor.dart';
 export 'src/widgets/link.dart' show LinkActionPickerDelegate, LinkMenuAction;
